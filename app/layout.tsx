fddc515a5b59c0742d0ba029127b4b182826a0ad
edengloria <0218sdh@gmail.com>
2025-06-30 11:09:36 +0900

    feat: 브라우저 탭 제목을 DareePayco로 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'DareePayco',
+  description: 'Dareesoft 페이코 점심 메뉴 뽑기',
   generator: 'v0.dev',
 }
 
